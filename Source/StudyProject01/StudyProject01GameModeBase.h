50fabf219f5712b2e5d67f2d4850c0c123cd5dcb
risehyun <risehyun01@gmail.com>
2024-01-03 13:13:06 +0900

    [FEAT] 프로젝트 생성

---
+++ Source/StudyProject01/StudyProject01GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StudyProject01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STUDYPROJECT01_API AStudyProject01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
