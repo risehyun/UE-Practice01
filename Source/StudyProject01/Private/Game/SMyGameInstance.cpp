96bbc13364779b7b9232ed12fc0b400f13016cd2
risehyun <risehyun01@gmail.com>
2024-01-04 09:41:15 +0900

    [FEAT] 인터페이스 추가

--- Source/StudyProject01/Private/Game/SMyGameInstance.cpp
+++ Source/StudyProject01/Private/Game/SMyGameInstance.cpp
@@ -9,11 +9,7 @@
 
 USMyGameInstance::USMyGameInstance()
 {
-	UE_LOG(LogTemp, Log, TEXT("USMyGameInstance() has been called."));
 
-	Name = TEXT("USGameInstance Class Default Object");
-	// CDO의 Name 속성에 저장됨.
-	// 중단점을 걸어보면 언리얼 에디터가 실행되기 전에 호출됨을 알 수 있음.
 }
 
 void USMyGameInstance::Init()
@@ -21,34 +17,17 @@ void USMyGameInstance::Init()
 	// 가상함수를 오버라이드한 함수를 작성할 때는 항상 Super 키워드의 Init을 사용해 언리얼 기능을 먼저 실행하도록 한다.
 	Super::Init();
 
-    USUnrealObjectClass* USObject1 = NewObject<USUnrealObjectClass>();
-    // 언리얼은 이런식으로 new 키워드를 안쓰고 NewObject<>() API를 사용해야 함.
+    USPigeon* Pigeon1 = NewObject<USPigeon>();
+    ISFlyable* Bird1 = Cast<ISFlyable>(Pigeon1);
+    // 현업에서 인터페이스 개념은 대부분 이런식으로 업캐스팅 하기 위함.
 
-    UE_LOG(LogTemp, Log, TEXT("USObject1's Name: %s"), *USObject1->GetName());
-    // 우리가 정의한 Getter()
-
-    FProperty* NameProperty = USUnrealObjectClass::StaticClass()->FindPropertyByName(TEXT("Name"));
-    // 프로퍼티 시스템을 활용한 Getter()
-
-    FString CompiletimeUSObjectName;
-    if (nullptr != NameProperty)
+    if (nullptr != Bird1)
     {
-        NameProperty->GetValue_InContainer(USObject1, &CompiletimeUSObjectName);
-        UE_LOG(LogTemp, Log, TEXT("CompiletimeUSObjectName: %s"), *CompiletimeUSObjectName);
+        Bird1->Fly();
     }
-
-    USObject1->HelloUnreal();
-
-    UFunction* HelloUnrealFunction = USObject1->GetClass()->FindFunctionByName(TEXT("HelloUnreal"));
-    if (nullptr != HelloUnrealFunction)
-    {
-        USObject1->ProcessEvent(HelloUnrealFunction, nullptr);
-    }
-
 }
 
 void USMyGameInstance::Shutdown()
 {
 	Super::Shutdown();
-	UE_LOG(LogTemp, Log, TEXT("Shutdown() has been called."));
 }
--- Source/StudyProject01/Public/Example/SFlyable.h
+++ Source/StudyProject01/Public/Example/SFlyable.h
@@ -6,6 +6,35 @@
 #include "UObject/Interface.h"
 #include "SFlyable.generated.h"
 
+USTRUCT()
+struct FBirdData
+{
+    GENERATED_BODY()
+
+public:
+    FBirdData() {}
+
+    FBirdData(const FString& InName, int32 InID)
+        : Name(InName)
+        , ID(InID)
+    {
+    }
+
+    friend FArchive& operator<<(FArchive& Ar, FBirdData& InBirdData)
+    {
+        Ar << InBirdData.Name;
+        Ar << InBirdData.ID;
+        return Ar;
+    }
+
+    UPROPERTY()
+    FString Name = TEXT("DefaultBirdName");
+
+    UPROPERTY()
+    int32 ID = 0;
+
+};
+
 // This class does not need to be modified.
 // 클래스 정보를 전달할 때 사용하게 되는 U 클래스이다.
 UINTERFACE(MinimalAPI)
