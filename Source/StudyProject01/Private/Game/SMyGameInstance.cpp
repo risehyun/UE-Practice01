a5dd0e928a6b7f7bf7d707910d698cc5721bcf8b
risehyun <risehyun01@gmail.com>
2024-01-04 15:11:03 +0900

    [FEAT] TSet 실습

--- Source/StudyProject01/Private/Game/SMyGameInstance.cpp
+++ Source/StudyProject01/Private/Game/SMyGameInstance.cpp
@@ -19,45 +19,52 @@ void USMyGameInstance::Init()
 	// 가상함수를 오버라이드한 함수를 작성할 때는 항상 Super 키워드의 Init을 사용해 언리얼 기능을 먼저 실행하도록 한다.
 	Super::Init();
 
-    const int32 ArraySize = 10;
-    TArray<int32> IntArray;
+    const int32 SetSize = 10;
+    TSet<int32> IntSet;
 
-    for (int32 i = 1; i <= ArraySize; ++i)
+    for (int32 i = 1; i <= SetSize; ++i)
     {
-        IntArray.Add(i);
+        IntSet.Add(i);
     }
 
-    for (int32 Element : IntArray)
-    {
-        static int32 i = 0;
-        UE_LOG(LogTemp, Log, TEXT("[%d]: %d"), i++, Element);
-    }
-    UE_LOG(LogTemp, Log, TEXT("====="));
-
-    IntArray.RemoveAll([](int32 InElement)->bool { return 0 == InElement % 2; });
-
-    for (int32 Element : IntArray)
+    for (int32 Element : IntSet)
     {
         static int32 i = 0;
         UE_LOG(LogTemp, Log, TEXT("[%d]: %d"), i++, Element);
     }
     UE_LOG(LogTemp, Log, TEXT("====="));
 
-    IntArray += {2, 4, 6, 8, 10};
+    IntSet.Remove(2);
+    IntSet.Remove(4);
+    IntSet.Remove(6);
+    IntSet.Remove(8);
+    IntSet.Remove(10);
 
-    for (int32 Element : IntArray)
+    for (int32 Element : IntSet)
     {
         static int32 i = 0;
         UE_LOG(LogTemp, Log, TEXT("[%d]: %d"), i++, Element);
     }
     UE_LOG(LogTemp, Log, TEXT("====="));
 
-    TArray<int32> IntArrayCompare;
-    IntArrayCompare.Init(0, ArraySize);
-    int32 IntArrayOriginal[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
-    FMemory::Memcpy(IntArrayCompare.GetData(), IntArrayOriginal, sizeof(int32) * ArraySize);
+    IntSet.Add(2);
+    IntSet.Add(4);
+    IntSet.Add(6);
+    IntSet.Add(8);
+    IntSet.Add(10);
 
-    UE_LOG(LogTemp, Log, TEXT("IntArray == IntArrayCompare ? %d"), IntArray == IntArrayCompare);
+    for (int32 Element : IntSet)
+    {
+        static int32 i = 0;
+        UE_LOG(LogTemp, Log, TEXT("[%d]: %d"), i++, Element);
+    }
+    UE_LOG(LogTemp, Log, TEXT("====="));
+
+    int32 Key = 2;
+    UE_LOG(LogTemp, Log, TEXT("%d: %s"), Key, nullptr == IntSet.Find(Key) ? TEXT("nullptr") : TEXT("Is in"));
+
+    Key = 11;
+    UE_LOG(LogTemp, Log, TEXT("%d: %s"), Key, nullptr == IntSet.Find(Key) ? TEXT("nullptr") : TEXT("Is in"));
 }
 
 void USMyGameInstance::Shutdown()
